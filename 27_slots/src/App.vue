d282febedfb9f075760965e5b84ac9e9a496d2c5
Philip Loa <boyet007@gmail.com>
2018-08-05 01:01:48 +0700

    27_slots

---
+++ 27_slots/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>  
+      <!-- <form-helper>
+        <h2 slot="title">{{ title }}</h2>
+        <p slot="text">I am the paragraph of the slot</p>
+      </form-helper> -->
+
+      <form-helper>
+        <div slot="form-header">
+          <h3>This is the title of the form</h3>
+          <p>Information about the form</p>
+        </div>
+        <div slot="form-fields">
+          <input type="text" name="" id="" placeholder="name" required>
+          <input type="password" name="" id="" placeholder="password" required>  
+        </div>
+        <div slot="form-controls">
+          <button v-on:click="handleSubmit">Submit</button>
+        </div>
+      </form-helper>
+  </div>
+</template>
+<script>
+
+import formHelper from './components/formHelper.vue'
+
+export default {
+  components: {
+    'form-helper': formHelper
+  },
+  data() {
+    return {
+      title: 'I am dynamic slot tile'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 27_slots/src/components/formHelper.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <!-- <slot name="title"></slot>
+        <h1>I am the form helper</h1>
+        <slot name="text"></slot> -->
+
+        <h1>Please fill our form</h1>
+        <form action="">
+            <div id="form-header">
+                <slot name="form-header"></slot>
+            </div>
+            <div id="form-fields">
+                <slot name="form-fields"></slot>
+            </div>
+            <div id="form-controls">
+                <slot name="form-controls"></slot>
+            </div>
+            <div id="usefull-links">
+                <ul>
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                    <li><a href="#">Link 4</a></li>
+                </ul>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+
+    }, 
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        text-align: center;
+    }
+
+    form{
+        width: 100%;
+        max-width: 960px;
+        margin: 0 auto;
+    }
+
+    #usefull-links ul{
+        padding:0;
+    }
+    #usefull-links li{
+        display: inline-block;
+        margin-right: 10px;
+    }
+
+    form > div {
+        padding:20px;
+        background: #eee;
+        margin:20px 0;   
+    }
+
+    #form-header{
+        background: #ddd;
+        border: 1px solid #bbb;
+    }
+</style>
