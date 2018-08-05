a51045c7fdcfaffe36902f9ba3ea88404283eb35
Philip Loa <boyet007@gmail.com>
2018-08-05 12:45:40 +0700

    28_dynamic_components

---
+++ 28_dynamic_components/src/components/formOne.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <form-helper>
+            <div slot="form-header">
+            <h3>Form One - Contact Us</h3>
+            <p>Fill in this form to contact us</p>
+            </div>
+            <div slot="form-fields">
+                <input type="text" name="" placeholder="name" required id="">
+                <label for="">Your message:</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div slot="form-controls">
+                <button v-on:click="handleSubmit">Send</button>
+            </div>
+        </form-helper>    
+    </div>
+</template>
+
+<script>
+
+import formHelper from './formHelper.vue'
+
+export default {
+    components: {
+        'form-helper': formHelper
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        handleSubmit: function() {
+            alert('Thanks for submiting form one & contacting us');
+        }
+    }
+}
+</script>
+
+<style>
+    body{
+        margin: 0;
+      
+    }
+</style>
---
+++ 28_dynamic_components/src/components/formTwo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <form-helper>
+            <div slot="form-header">
+                <h3>Form Two - Log in</h3>
+                <p>Enter your details to log in</p>
+            </div>
+            <div slot="form-fields">
+                <input type="text" name="" placeholder="username" required id="">
+                <input type="password" name="" placeholder="password" required id="">
+            </div>
+            <div slot="form-controls">
+                <button v-on:click="handleSubmit">Login</button>
+            </div>
+        </form-helper>
+    </div>
+</template>
+
+<script>
+
+import formHelper from './formHelper.vue'
+
+export default {
+    components: {
+        'form-helper': formHelper
+    },
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        handleSubmit: function() {
+            alert('thanks for logging in (form-two)');
+        }
+    } 
+}
+</script>
+
+<style>
+    body{
+        margin: 0;
+        
+    }
+</style>
+
