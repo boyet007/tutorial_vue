3e0fb39e0dd222a3f497541120bf8ff89d7521cc
Philip Loa <boyet007@gmail.com>
2018-08-05 23:31:12 +0700

    44_retrieving_posts_from_firebase (FINISHED)

---
+++ 44_retrieving_posts_from_firebase/src/components/singleBlog.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="single-blog">
+        <h1>{{ blog.title }}</h1>
+        <article>{{ blog.content }}</article>
+        <p>Author: {{ blog.author }}</p>
+        <ul>
+            <li v-for="category in blog.categories">{{ category }}</li>
+        </ul>
+    </div>    
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            id: this.$route.params.id,
+            blog: {}
+        }
+    },
+    created()  {
+        this.$http.get('https://belajarvue-9c342.firebaseio.com/posts/' + this.id + '.json').then(function(data) {
+            //console.log(data);
+            return data.json();
+
+        }).then(function(data){
+            this.blog = data;
+        });
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
