b6c4bf03f28146d3ad3bcbc8c08416522fff6bcc
Philip Loa <boyet007@gmail.com>
2018-08-05 19:35:41 +0700

    35_filters

---
+++ 35_filters/src/components/showBlogs.vue
@@ -0,0 +1,45 @@
+<template>
+  <div v-theme:column="'narrow'" id="show-blogs">
+      <h1>All Blog Articles</h1>
+      <div v-for="blog in blogs" class="single-blog">
+          <h2 v-rainbow>{{ blog. title | to-uppercase }}</h2>
+          <article>{{ blog.body | snippet }}</article>
+      </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            blogs: []
+        }
+    },
+    methods: {
+        
+    },
+
+    //created di jalankan ketika komponen dibuat pertama kali.
+    created() {
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data) {
+            this.blogs = data.body.slice(0, 10);
+        });        
+    }
+}
+</script>
+
+<style>
+    #show-blogs{
+        max-width: 800px;
+        margin: 0 auto;
+    }
+
+    .single-blog{
+        padding: 20px;
+        margin: 20px 0;
+        box-sizing: border-box;
+        background: #eee;
+    }
+</style>
