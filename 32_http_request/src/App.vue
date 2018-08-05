9f23ba9d5cda1b4d03fb413a0fc0e4c4c5e6cbea
Philip Loa <boyet007@gmail.com>
2018-08-05 14:31:46 +0700

    32_http_request

---
+++ 32_http_request/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue';
+
+export default {
+  components: {
+    'add-blog': addBlog
+  }
+}
+</script>
+
+<style>
+
+</style>
