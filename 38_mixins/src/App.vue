2ecd17f1bfc0c5a144843f8491da03e2a60b3f89
Philip Loa <boyet007@gmail.com>
2018-08-05 21:14:18 +0700

    38_mixins

---
+++ 38_mixins/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+<show-blogs></show-blogs>
+<list-blogs></list-blogs>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue';
+import showBlogs from './components/showBlogs.vue';
+import listBlogs from './components/listBlogs.vue';
+
+export default {
+  components: {
+    'add-blog': addBlog,
+    'show-blogs': showBlogs,
+    'list-blogs': listBlogs
+  }
+}
+</script>
+
+<style>
+ 
+</style>
