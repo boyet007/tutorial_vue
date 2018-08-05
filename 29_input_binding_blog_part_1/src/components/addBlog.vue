a43dde54c996c6794b70a594e9e8feb84f670e83
Philip Loa <boyet007@gmail.com>
2018-08-05 12:45:40 +0700

    fix_28_dynamic_components

--- 29_input_binding_blog_part_1/src/components/addBlog.vue
+++ 29_input_binding_blog_part_1/src/components/addBlog.vue
@@ -1,56 +1,86 @@
 <template>
-  <div id="add-blog">
-      <h2>Add a new Blog Post</h2>
-      <form>
-          <label for="">Blog Title: </label>
-          <input type="text" v-model.lazy="blog.title" name="" required id="">
-          <label for="">Blog Content</label>
-          <textarea name="" id="" v-model.lazy="blog.content" cols="30" rows="10"></textarea>
-      </form>
-      <div id="preview">
-          <h3>Preview Blog</h3>
-          <p>Blog Title: {{ blog.title }} </p>
-          <p>Blog Content:</p> <p>{{ blog.content }} </p>
-      </div>
-
-  </div>
+    <div id="add-blog">
+        <h2>Add a New Blog Post</h2>
+        <form action="">
+            <label for="">Blog Title:</label>
+            <!-- v-model.lazy = update ketika input lost focus -->
+            <input type="text" v-model.lazy="blog.title" name="" id="" required/>
+            <label for="">Blog Content</label>
+            <textarea name="" id="" cols="30" v-model.lazy="blog.content" rows="10"></textarea>
+            <div id="checkboxes">
+                <label for="">Ninjas</label>
+                <input type="checkbox" value="ninjas" v-model="blog.categories">
+                <label for="">Wizards</label>
+                <input type="checkbox" value="wizards" v-model="blog.categories" >
+                <label for="">Mario</label>
+                <input type="checkbox" value="mario" v-model="blog.categories">
+                <label for="">Cheese</label>
+                <input type="checkbox" value="cheese" v-model="blog.categories">
+            </div>    
+        </form>
+        <div id="preview">
+            <h3>Preview Blog</h3>
+            <p>Blog Title: {{ blog.title }}</p>
+            <p>Blog content: </p>
+            <p>{{ blog.content }}</p>
+            <p>Blog categories: </p>
+            <ul>
+                <li v-for="category in blog.categories">{{ category }}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            blog: {
-                title: '',
-                content: ''
-            }
+  data() {
+    return {
+      blog: {
+        title: "",
+        content: "",
+        categories: [
 
-        }
-    }
-}
+        ]
+      }
+    };
+  }
+};
 </script>
 
 <style>
-    #add-blog *{
-        box-sizing: border-box;
-    }
+#add-blog * {
+  box-sizing: border-box;
+}
 
-    #add-blog{
-        margin: 20px auto;
-        max-width: 500px;
-    }
-    label{
-        display: block;
-        margin:20px 0 10px;
-    }
-    input[type="text"], textarea{
-        display: block;
-        width: 100%;
-    }
-    #preview{
-        padding: 10px 20px;
-        border: 1px dotted #ccc;
-        margin:30px 0;
+#add-blog {
+  margin: 20px auto;
+  max-width: 500px;
+}
+label {
+  display: block;
+  margin: 20px 0 10px;
+}
+input[type="text"],
+textarea {
+  display: block;
+  width: 100%;
+}
+#preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
+}
 
-    }
+h3{
+    margin-top:10px;
+}
+
+#checkboxes input {
+    display:inline-block;
+    margin-right:10px;
+}
+
+#checkboxes label {
+    display: inline-block;
+}
 </style>
