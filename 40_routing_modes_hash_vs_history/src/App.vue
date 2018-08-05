11f86ae0d5a972c18b285d87597369e584fac6fa
Philip Loa <boyet007@gmail.com>
2018-08-05 21:15:00 +0700

    40_routing_modes_hash_vs_history

---
+++ 40_routing_modes_hash_vs_history/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+<!-- <show-blogs></show-blogs>
+<list-blogs></list-blogs> -->
+
+<router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue';
+import showBlogs from './components/showBlogs.vue';
+import listBlogs from './components/listBlogs.vue';
+
+export default {
+  components: {
+    'add-blog': addBlog,
+    'show-blogs': showBlogs,
+    'list-blogs': listBlogs
+  }
+}
+</script>
+
+<style>
+ 
+</style>
