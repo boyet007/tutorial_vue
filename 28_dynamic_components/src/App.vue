d65d80c7b543c258cbbdb4e0e5cd7fb5e07403aa
Philip Loa <boyet007@gmail.com>
2018-08-20 15:05:51 +0700

    fix_28_dynamic_components_update

--- 28_dynamic_components/src/App.vue
+++ 28_dynamic_components/src/App.vue
@@ -1,37 +1,35 @@
 <template>
-  <div>
-    <keep-alive>
-    <component v-bind:is="component"></component>
-    </keep-alive>
-    <button v-on:click="component = 'form-one'">Show form one</button>
-    <button v-on:click="component = 'form-two'">Show form two</button>
-  </div>
+<div>
+  <!-- <form-one></form-one> -->
+  <!-- <form-two></form-two> -->
+
+<!-- hasil sama seperti diatas -->
+<keep-alive>
+  <component v-bind:is="component"></component>
+  <!-- keep alive untuk supaya yang diinput tidak hilang ketika pergantiann form -->
+  </keep-alive>
+  <button v-on:click="component = 'form-one'">Show form one</button>
+ <button v-on:click="component = 'form-two'">Show form two</button>  
+</div>
 </template>
 
 <script>
-
-import formOne from './components/formOne.vue'
-import formTwo from './components/formTwo.vue'
-
-
+import formOne from "./components/formOne.vue";
+import formTwo from "./components/formTwo.vue";
 
 export default {
   components: {
-     'form-one': formOne,
-     'form-two': formTwo
+    "form-one": formOne,
+    "form-two": formTwo
   },
-    data () {
+  data() {
     return {
       component: 'form-two'
-    }
+    };
   },
-  methods: {
-
-  }
-}
+  methods: {}
+};
 </script>
 
 <style>
-
-
 </style>
