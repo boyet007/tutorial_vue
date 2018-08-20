c1485d4adc7a18edfcf6f954e3ab2df6112edca2
Philip Loa <boyet007@gmail.com>
2018-08-20 16:26:21 +0700

    fix_32_http_request

--- 31_select_box_binding/src/components/addBlog.vue
+++ 31_select_box_binding/src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="add-blog">
       <h2>Add a new Blog Post</h2>
-      <form>
+      <form v-if="!submitted">
           <label for="">Blog Title: </label>
           <input type="text" v-model.lazy="blog.title" name="" required id="">
           <label for="">Blog Content</label>
@@ -20,7 +20,12 @@
             <select v-model="blog.author">
                 <option v-for="author in authors">{{ author }}</option>
             </select>
+            <button v-on:click.prevent="post">Add Blog</button>
       </form>
+      <div v-if="submitted">
+
+          <h3>Thanks for adding your post</h3>
+      </div>
       <div id="preview">
           <h3>Preview Blog</h3>
           <p>Blog Title: {{ blog.title }} </p>
@@ -31,6 +36,7 @@
           </ul>
           <p>Authors: {{ blog.author }}</p>
       </div>
+
 
   </div>
 </template>
@@ -45,7 +51,20 @@ export default {
                 categories: [],
                 author: ''
             },
-            authors: ['The Net Ninja', 'The Angular Evenger', 'The Vue Vindicator']
+            authors: ['The Net Ninja', 'The Angular Evenger', 'The Vue Vindicator'],
+            submitted: false
+        }
+    },
+    methods: {
+        post:function() {
+            this.$http.post('https://jsonplaceholder.typicode.com/posts', {
+                title: this.blog.title,
+                body: this.blog.content,
+                userId: 1
+            }).then(function(data) {
+                console.log(data);
+                this.submitted=true;
+            });
         }
     }
 }
