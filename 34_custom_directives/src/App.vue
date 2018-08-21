86cea9c1fd61240826892be3461c92e3f5fc52e2
Philip Loa <boyet007@gmail.com>
2018-08-21 14:53:54 +0700

    fix_41_add_route_links

--- 34_custom_directives/src/App.vue
+++ 34_custom_directives/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
+    
 <!-- <show-blogs></show-blogs>
 <list-blogs></list-blogs> -->
 
 <!-- kalau pakai route harus deklaariskan router view di app vue -->
+  <app-header></app-header>
   <router-view></router-view>
   </div>
 </template>
@@ -16,15 +18,17 @@
 // import showBlogs from './components/showBlogs.vue';
 // import listBlogs from './components/listBlog.vue';
 
+//pakai componen app-header
+import header from './components/header.vue';
 
-
-// export default {
-//   components: {
-//     'add-blog': addBlog,
-//     'show-blogs': showBlogs,
-//     'list-blogs': listBlogs
-//   }
-// }
+export default {
+  components: {
+    // 'add-blog': addBlog,
+    // 'show-blogs': showBlogs,
+    // 'list-blogs': listBlogs
+    'app-header' :header
+  }
+}
 </script>
 
 <style>
---
+++ 34_custom_directives/src/components/header.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+    <nav>
+        <ul>
+            <!-- router linke pengganti a href -->
+            <!-- exact untuk toggle background ketika link active berubah -->
+            <li><router-link to="/" exact="">Blog</router-link></li>
+            <li><router-link to="/add" exact="">Add a New Blog</router-link></li>
+            <!-- <li><router-link to="/add">Add a New Blog</li> -->
+        </ul>
+    </nav>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+nav {
+    background: #444;
+    padding: 14px 0;
+    margin-bottom: 40px;
+}
+
+a {
+    color: #fff;
+    text-decoration: none;
+    padding:6px 8px;
+    border-radius:10px;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+ul {
+    list-style-type: none;
+    text-align: center;
+    margin: 0;
+}
+
+.router-link-active{
+    background: #eee;
+    color: #444;
+}
+</style>
